4a318b9f2dbb9be26171fb9c18002518150322cb
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-08-29 21:34:33 -0300

    Create the React Router for pages

---
+++ costs/src/App.js
@@ -0,0 +1,39 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
+
+function App() {
+  return (
+
+    <Router>
+
+      <div>
+      <ul>
+        <Link to="/" >Home</Link>
+        <Link to="/contact" >Contato</Link>
+        <Link to="/company" >Empresa</Link>
+        <Link to="/newproject" >Novo Projeto</Link>
+      </ul>
+      </div>
+
+      <Container customClass='min-height'>
+      <Routes>
+        <Route path="/" element= {< Home />}/>
+        <Route path="/company" element= {< Company />}/>
+        <Route path="/contact" element= {< Contact />}/>
+        <Route path="/newproject" element= {< NewProject />}/>
+      </Routes>
+      </Container>
+
+      <footer>Footer</footer>
+
+    </Router>
+  );
+}
+
+export default App;
