9963ab34c4885e23ab0e3fc8c7a57d5f24f5cb53
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-09-02 21:57:25 -0300

    Remove projects

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -14,6 +14,7 @@ function Projects(){
 
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -48,8 +49,9 @@ function Projects(){
                 'Content-type': 'application.json'
             },
         }).then(resp => resp.json()) 
-        .then(data => {
+        .then(() => {
             setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
         })
         .catch(err => console.log(err))
             
@@ -63,6 +65,7 @@ function Projects(){
                 <LinkButton to='/newproject' text='Criar Projeto'/>
             </div>
            {message &&  <Message type='sucess' msg={message}/>}
+           {projectMessage &&  <Message type='sucess' msg={projectMessage}/>}
            <Container customClass='start'>
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -72,6 +75,7 @@ function Projects(){
                         budget={project.budget}
                         category={project.category.name}
                         key={project.id}
+                        handleRemove={removeProject}
                         />
                     ))}
                     {!removeLoading && <Loading />}
