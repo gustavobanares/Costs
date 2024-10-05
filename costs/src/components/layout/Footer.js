307439ffb214d6b3f88c8a8f278c894107d33784
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-05 15:13:12 -0300

    Adding my social medias

--- costs/src/components/layout/Footer.js
+++ costs/src/components/layout/Footer.js
@@ -7,11 +7,15 @@ function Footer(){
     return(
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
-                <li>
-                    <FaGithub />
+            <li>
+                    <a href="https://github.com/gustavobanares" target="_blank" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
                 </li>
                 <li>
-                    <FaLinkedin />
+                    <a href="https://www.linkedin.com/in/gustavo-ba%C3%B1ares-de-bacco-76467b268/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin />
+                    </a>
                 </li>
             </ul>
             <p className={styles.copy_right}><span>Costs</span> &copy; 2024</p>
